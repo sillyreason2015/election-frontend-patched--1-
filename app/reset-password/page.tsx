f9a3d890cf28c9b6aa9212fc96a5c15f9147223c
sillyreason2015 <njatau@outlook.com>
2025-08-29 10:03:04 +0100

    updated to preven prerendering

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,16 +1,10 @@
 'use client'
 
-import { Suspense } from "react"
-import ResetPasswordPage from "./resetPasswordPage"
+import ResetPasswordPage from './resetPasswordPage'
 
+// Force this route to render only at runtime
+export const dynamic = 'force-dynamic'
 
-export const dynamic = "force-dynamic"
-
-
-export default function ResetPasswordPageWrapper() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ResetPasswordPage/>
-    </Suspense>
-  )
+export default function Page() {
+  return <ResetPasswordPage />
 }
